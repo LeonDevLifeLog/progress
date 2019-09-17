ee6faafb99b3bed84b6dc4f756ab238ffa5686fa
Leon <leondevlifelog@gmail.com>
2019-09-17 08:35:34 +0800

    去掉调试日志

--- progress/src/main/java/com/github/leondevlifelog/progress/Progress.java
+++ progress/src/main/java/com/github/leondevlifelog/progress/Progress.java
@@ -10,7 +10,6 @@ import android.graphics.RectF;
 import android.graphics.Shader;
 import android.graphics.SweepGradient;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 
@@ -142,7 +141,6 @@ public class Progress extends View {
                 Float degree = (Float) valueAnimator.getAnimatedValue();
                 scanDegree = degree - (degree % 3.6f);
                 invalidate();
-                Log.d(TAG, "onAnimationUpdate: " + degree);
             }
         });
     }
